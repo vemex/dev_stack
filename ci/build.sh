55999d3e217717b9d54a2b30a2f2c846f90ad5ac
WANG WEI WEI <weiwei.wang-ww@siemens.com>
2018-09-06 16:56:39 +0800

    update ci

--- ci/build.sh
+++ ci/build.sh
@@ -1,8 +1,9 @@
 mkdocs build
+ls
 git clone  --depth 50 --branch gh-pages http://${GH_REF}  dev-stack-pages
 mkdir -p nicety-pages/reporter/test
 mkdir -p nicety-pages/reporter/coverage
-cp -r dev_stack/site/*  dev-stack-pages/ 
+cp -r ./site/*  dev-stack-pages/ 
 cd dev-stack-pages
 git add .
 git commit -m "[auto]deliver docs"
