ac5ccca45c879e0e80cbfda91f34f8a4f97dd902
WANG WEI WEI <weiwei.wang-ww@siemens.com>
2018-09-06 16:39:23 +0800

    add ci

--- ci/build.sh
+++ ci/build.sh
@@ -1,11 +1,9 @@
 mkdocs build
-
- git clone  --depth 50 --branch gh-pages http://github.com/vemex/Nicety.git  nicety-pages
-    mkdir -p nicety-pages/reporter/test
-    mkdir -p nicety-pages/reporter/coverage
-    cp -r Nicety/target/test/*  nicety-pages/reporter/test
-    cp -r Nicety/target/coverage/html/*  nicety-pages/reporter/coverage
-    cd nicety-pages
-    git add .
-    git commit -m "[auto]deliver reporter"
-    git push --force --quiet "https://${GH_TOKEN}@${GH_REF}" HEAD:gh-pages
+git clone  --depth 50 --branch gh-pages http://${GH_REF}  dev-stack-pages
+mkdir -p nicety-pages/reporter/test
+mkdir -p nicety-pages/reporter/coverage
+cp -r dev_stack/site/*  dev-stack-pages/ 
+cd dev-stack-pages
+git add .
+git commit -m "[auto]deliver docs"
+git push --force --quiet "https://${GH_TOKEN}@${GH_REF}" HEAD:gh-pages
