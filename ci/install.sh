96943d6c4cacc4359f3dec56e2c3805f561eb357
WANG WEI WEI <vivlanwong120729@gmail.com>
2018-09-11 17:33:45 +0800

    update ci

--- ci/install.sh
+++ ci/install.sh
@@ -2,8 +2,8 @@ set -e
 PYTHON_PACKAGES_DIR=`python -c "from distutils.sysconfig import get_python_lib; print get_python_lib()"`
 MKDOCS_DIR="$PYTHON_PACKAGES_DIR/mkdocs"
 
-cp -i ./__init__.py "$MKDOCS_DIR/contrib/search/__init__.py"
-cp -i ./lunr.jp.py "$MKDOCS_DIR/contrib/search/lunr-language/lunr.jp.js"
-cp -i ./prebuild-index.js "$MKDOCS_DIR/contrib/search/prebuild-index.js"
-cp -i ./tinyseg.js "$MKDOCS_DIR/contrib/search/lunr-language/tinyseg.js"
-cp -i ./worker.js "$MKDOCS_DIR/contrib/search/templates/search/worker.js" 
+cp -i ./ci/__init__.py "$MKDOCS_DIR/contrib/search/__init__.py"
+cp -i ./ci/lunr.jp.py "$MKDOCS_DIR/contrib/search/lunr-language/lunr.jp.js"
+cp -i ./ci/prebuild-index.js "$MKDOCS_DIR/contrib/search/prebuild-index.js"
+cp -i ./ci/tinyseg.js "$MKDOCS_DIR/contrib/search/lunr-language/tinyseg.js"
+cp -i ./ci/worker.js "$MKDOCS_DIR/contrib/search/templates/search/worker.js" 
