e330f8fe17bb82b2d20c016325a6058236906a5e
WANG WEI WEI <vivlanwong120729@gmail.com>
2018-09-11 17:36:15 +0800

    update ci

--- ci/install.sh
+++ ci/install.sh
@@ -2,8 +2,8 @@ set -e
 PYTHON_PACKAGES_DIR=`python -c "from distutils.sysconfig import get_python_lib; print get_python_lib()"`
 MKDOCS_DIR="$PYTHON_PACKAGES_DIR/mkdocs"
 
-cp -i ./ci/__init__.py "$MKDOCS_DIR/contrib/search/__init__.py"
-cp -i ./ci/lunr.jp.py "$MKDOCS_DIR/contrib/search/lunr-language/lunr.jp.js"
-cp -i ./ci/prebuild-index.js "$MKDOCS_DIR/contrib/search/prebuild-index.js"
-cp -i ./ci/tinyseg.js "$MKDOCS_DIR/contrib/search/lunr-language/tinyseg.js"
-cp -i ./ci/worker.js "$MKDOCS_DIR/contrib/search/templates/search/worker.js" 
+yes | cp -rf ./ci/__init__.py "$MKDOCS_DIR/contrib/search/__init__.py"
+yes | cp -rf ./ci/lunr.jp.py "$MKDOCS_DIR/contrib/search/lunr-language/lunr.jp.js"
+yes | cp -rf ./ci/prebuild-index.js "$MKDOCS_DIR/contrib/search/prebuild-index.js"
+yes | cp -rf ./ci/tinyseg.js "$MKDOCS_DIR/contrib/search/lunr-language/tinyseg.js"
+yes | cp -rf ./ci/worker.js "$MKDOCS_DIR/contrib/search/templates/search/worker.js" 
